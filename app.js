09111e38fbb4588f9d62b573a9234e2555aaa18d
sz4papdav <sz4papdav@SZT5-12-HP>
2025-09-15 11:03:47 +0200

    POST kesz

--- app.js
+++ app.js
@@ -1,7 +1,45 @@
-import express from "express"
+import express, { response } from "express"
 
 const PORT = 3000
 const app = express()
+
+app.use(express.json());
+const users = [
+  { id: 1, name: "Abel", age: 19 },
+  { id: 2, name: "Bob", age: 20 },
+  { id: 3, name: "Cedric", age: 21 },
+  { id: 4, name: "Dave", age: 22 },
+];
+
+//GET
+    app.get("/users", (req, res) =>{
+        res.status(200).json(users)
+    })
+
+    app.get("/users/:id", (req, res) => {
+        const userId = +req.params.id
+        const user = users.find(user => user.id == userId)
+        if(!user)
+        {
+            return res.status(404).json({message: "User not found"})
+        }
+        res.status(200).json(user);
+    });
+//POST
+    app.post("/users", (req, res) => {
+      const name = req.body.name;
+      const age = +req.body.age;
+      if (!name || !age) {
+        return res.status(400).json({ message: "Invalid credentials" });
+      }
+      const id = users[users.length -1]?.id + 1 || 1;
+      const user = {id, name, age}
+      users.push(user)
+      res.status(201).json(user);
+    });
+//PUT
+//DELETE
+//PATCH
 
 app.listen(PORT, () =>{
     console.log(`Server runs on port http://localhost:${PORT}`)
